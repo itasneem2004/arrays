3937835bb5f1a2b77c09660251ab21aae7e1a831
itasneem2004 <idrisitasneembsk@gmail.com>
2024-06-25 17:58:03 +0530

    arraystutor

---
+++ arrays.js
@@ -0,0 +1,57 @@
+// Arrays in JavaScript are a fundamental data structure used to store collections of values. 
+// They can hold various data types, including numbers, strings, objects, functions, and even other arrays. 
+// Arrays in JavaScript are zero-indexed, which means the first element is at index 0, 
+// the second element is at index 1, and so on.
+
+// Creating an Array:
+// You can create an array using either array literals or the Array constructor.
+
+// Using array literals
+// const fruits = ["apple","mango","blueberry"];
+// .................. 0 ...... 1 ...... 2 ...
+
+//  Using the Array constructor
+// const colors =  ["red","yellow","blue"];
+
+// console.log(fruits[0]);
+// console.log(colors[2]);
+
+// Modifying Elements:
+// You can change the value of an array element by assigning a new value to it.
+
+// fruits[2] = "strawberry";
+// console.log(fruits);
+
+// Array Length:
+// You can find the number of elements in an array using the length property.
+
+// console.log(fruits.length);
+
+// Array Methods:
+// JavaScript provides a variety of methods to manipulate arrays. Some common array methods include:
+
+// push(): Add an element to the end of the array.
+// pop(): Remove the last element from the array.
+// unshift(): Add an element to the beginning of the array.
+// shift(): Remove the first element from the array.
+// concat(): Concatenate two or more arrays.
+// slice(): Create a new array by slicing a portion of an existing array.
+// splice(): Add or remove elements from an array at a specified position.
+// forEach(): Iterate over the array elements.
+// map(): Create a new array by applying a function to each element of an existing array.
+// filter(): Create a new array with elements that pass a specified condition.
+// reduce(): Reduce an array to a single value using a callback function.
+// find(): Find the first element that satisfies a condition.
+// indexOf(): Find the index of a specified element.
+// includes(): Check if an array contains a specified element.
+
+// push(): Adds one or more elements to the end of an array and returns the new length of the array.
+// const fruits = ["apple","mango"];
+// fruits.push("watermelon","cherry");
+// console.log(fruits);
+
+// pop(): Removes and returns the last element from an array.
+// const colors = ["magenta","cyan","purple"];
+// const lastcolor = colors.pop("purple");
+// console.log(lastcolor);
+
